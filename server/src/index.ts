40520ee3e31bd1cde7dd24076ac6052dacd5ced5
Atsuki Kitada <canvanvam@gmail.com>
2024-02-25 00:31:02 -0800

    set up cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 const port = 8000;
 
 app.get("/", (req, res) => {
