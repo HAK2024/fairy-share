b229d28ad156a604d1bd603b0ce6909774e4513b
Atsuki Kitada <canvanvam@gmail.com>
2024-02-28 01:33:13 -0800

    create getUser api

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { UsersModule } from './users/users.module';
-import { UsersModule } from './users/users.module';
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [UsersModule],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
