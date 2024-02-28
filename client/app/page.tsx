df44ae1189ae08dfa0d5597ca105441b9105a9cd
Kana Taguchi <mukrabwy103@gmail.com>
2024-02-27 17:20:42 -0800

    Test action2

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,10 @@
 import { Button } from '@/_components/ui'
 
 export default function Home() {
-  let test = 'test'
-
   return (
     <main className='p-24'>
       Hello World!
-      <p className="accent-sky-200 text-xs m-4 p-4">Text</p>
+      <p className='accent-sky-200 text-xs m-4 p-4'>Text</p>
       <div className='text-sm m-4 flex gap-10 p-4 accent-sky-200'>
         <Button variant={'destructive'}>Click me</Button>
         <Button variant={'outline'} disabled>
