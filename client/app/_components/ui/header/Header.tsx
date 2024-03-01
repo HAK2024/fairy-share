3423405d1d553be70f3495d2893e1c079df137a2
Atsuki Kitada <canvanvam@gmail.com>
2024-03-01 15:18:48 -0800

    adjust design

--- client/app/_components/ui/header/Header.tsx
+++ client/app/_components/ui/header/Header.tsx
@@ -5,17 +5,24 @@ const Header = () => {
   return (
     <div className='flex h-16 w-full items-center justify-between bg-primary px-4 text-primary-foreground md:h-20 md:px-14'>
       <Logo width={150} height={150} />
-      <div className='flex items-center justify-between gap-2'>
+      <div className='flex items-center justify-between gap-4'>
         {/* Navigation menu: Visible on devices wider than "md" breakpoint */}
-        <div className='hidden items-center justify-between gap-2 md:flex'>
+        <div className='hidden items-center justify-between gap-4 md:flex'>
           <Link href={'/'}>
-            <Button variant={'ghost'}>Home</Button>
+            {/* Figma design text-lg and font-bold feels a little too much */}
+            <Button variant={'ghost'} className='text-md font-semibold'>
+              Home
+            </Button>
           </Link>
           <Link href={'/calendar'}>
-            <Button variant={'ghost'}>Calendar</Button>
+            <Button variant={'ghost'} className='text-md font-semibold'>
+              Calendar
+            </Button>
           </Link>
           <Link href={'/expense'}>
-            <Button variant={'ghost'}>Expense</Button>
+            <Button variant={'ghost'} className='text-md font-semibold'>
+              Expense
+            </Button>
           </Link>
         </div>
         {/* End of navigation menu */}
