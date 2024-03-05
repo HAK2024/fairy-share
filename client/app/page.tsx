160cbf1849f279bfd78810aebcf0a22d6f3435bb
Atsuki Kitada <canvanvam@gmail.com>
2024-03-04 17:15:09 -0800

    Fix TODO about button components and adjust button size in Button component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,17 +5,7 @@ export default function Home() {
     <div>
       <Heading
         title='Fairy share'
-        // TODO: There might be a better way to switch between two different size of Button component depending on the screen size?
-        buttonComponent={() => (
-          <>
-            <Button variant={'outline'} className='block md:hidden'>
-              Rules
-            </Button>
-            <Button variant={'outline'} className='hidden md:block'>
-              Rules
-            </Button>
-          </>
-        )}
+        buttonComponent={() => <Button variant={'outline'}>Rules</Button>}
       />
     </div>
   )
