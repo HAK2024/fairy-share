88f0325371561a0abc6c0065a38521b8b4c5aed3
Kana Taguchi <mukrabwy103@gmail.com>
2024-03-06 17:44:02 -0800

    Delete codes

--- client/app/todos/api/getTodosApi.ts
+++
@@ -1,8 +0,0 @@
-import apiClient from '@/_api/base'
-import { UserType } from '@/_types'
-
-// TODO - replace with real API call of todos. For now, just returning GET/me data
-export const getTodosApi = async (): Promise<UserType> => {
-  const response = await apiClient.get<UserType>('/me')
-  return response.data
-}
