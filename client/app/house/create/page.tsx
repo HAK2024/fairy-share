40f8e3df2fdb3bc710c146be6055469be9eedf94
Kana Taguchi <mukrabwy103@gmail.com>
2024-03-13 13:29:44 -0700

    Fix client error

--- client/app/house/create/page.tsx
+++ client/app/house/create/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { Button } from '@/_components/ui'
-
 export default function HouseCreatePage() {
   return (
     <div className='px-4 pb-10 pt-8 md:px-14 md:pb-20 md:pt-10'>
