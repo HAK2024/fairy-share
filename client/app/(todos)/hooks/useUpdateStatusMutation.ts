3160c1d542a54421905181f9b034656dc1fcc526
Atsuki Kitada <canvanvam@gmail.com>
2024-03-13 23:52:51 -0700

    add error toast

--- client/app/(todos)/hooks/useUpdateStatusMutation.ts
+++ client/app/(todos)/hooks/useUpdateStatusMutation.ts
@@ -1,6 +1,8 @@
 import { useMutation } from '@tanstack/react-query'
 import { useQueryClient } from '@tanstack/react-query'
+import { toast } from '@/_hooks/useToast'
 import { useAuthStore } from '@/_stores'
+import { isErrorWithMessage } from '@/_utils'
 import { updateTaskStatusApi } from './api'
 
 export const useUpdateStatusMutation = () => {
@@ -14,7 +16,17 @@ export const useUpdateStatusMutation = () => {
       queryClient.invalidateQueries({ queryKey: ['todos', houseId] })
     },
     onError: (error) => {
-      console.error('Update failed', error)
+      console.error(error)
+      let message = 'Please try again later.'
+
+      if (isErrorWithMessage(error) && error.response) {
+        message = error.response.data.message
+      }
+      toast({
+        variant: 'destructive',
+        title: 'Failed to update task status..',
+        description: message,
+      })
     },
   })
 
