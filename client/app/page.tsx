38d451546e7f85f67065d9ffda6c7ee1c0ea268b
Atsuki Kitada <canvanvam@gmail.com>
2024-03-14 14:18:06 -0700

    delete no house error component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { Alert as NoHouseAlert, TodosList } from './(todos)/components'
+import { TodosList } from './(todos)/components'
 import { useGetTodosQuery } from './(todos)/hooks'
 import { Loading } from './_components/layout'
 import { Button, Heading } from './_components/ui'
@@ -20,28 +20,18 @@ export default function Home() {
   }
 
   return (
-    <div className='flex flex-col gap-8 px-4 pb-10 pt-8 text-slate-800 md:px-14 md:pb-20 md:pt-10 '>
-      {!todos ? (
-        <NoHouseAlert
-          mainMessage='You are currently not in any house.'
-          // TODO: Replace the linkHref with the actual path
-          linkHref='/create-setting'
-          linkText='Create house'
-          additionalText='or wait for an invitation'
+    todos && (
+      <div className='flex flex-col gap-8 px-4 pb-10 pt-8 text-slate-800 md:px-14 md:pb-20 md:pt-10 '>
+        <Heading
+          title={todos.houseName}
+          buttonComponent={() => (
+            <Button variant={'outline'} onClick={() => router.push('/rules')}>
+              Rules
+            </Button>
+          )}
         />
-      ) : (
-        <>
-          <Heading
-            title={todos.houseName}
-            buttonComponent={() => (
-              <Button variant={'outline'} onClick={() => router.push('/rules')}>
-                Rules
-              </Button>
-            )}
-          />
-          <TodosList todos={todos} />
-        </>
-      )}
-    </div>
+        <TodosList todos={todos} />
+      </div>
+    )
   )
 }
