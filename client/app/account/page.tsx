33cde33c0f7eda2d7d410b0a79af665154f56b44
Hyejin Seok <hyejin.seok7@gmail.com>
2024-03-18 14:34:19 -0700

    Add edit icon

--- client/app/account/page.tsx
+++ client/app/account/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React from 'react'
+import { FiEdit } from 'react-icons/fi'
 import { FormContainer } from '@/_components/layout'
 import { Button, Heading } from '@/_components/ui'
 import { AccountInfo } from './component'
@@ -11,7 +12,12 @@ export default function Account() {
       <div className='flex flex-col gap-8 md:gap-6 '>
         <Heading
           title='Account'
-          buttonComponent={<Button variant={'outline'}>Edit</Button>}
+          buttonComponent={
+            <Button variant={'outline'}>
+              <FiEdit />
+              &nbsp;Edit
+            </Button>
+          }
         />
         <AccountInfo />
         <div>
