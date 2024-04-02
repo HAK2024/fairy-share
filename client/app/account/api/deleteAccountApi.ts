6b22c50aa797734e16c69fe34f55f4c8b2280390
Hyejin Seok <hyejin.seok7@gmail.com>
2024-04-02 16:31:00 -0700

    Add query houseId and seed user

--- client/app/account/api/deleteAccountApi.ts
+++ client/app/account/api/deleteAccountApi.ts
@@ -1,6 +1,8 @@
 import apiClient from '@/_api/base'
 
 export const deleteAccountApi = async (houseId: number) => {
-  const response = await apiClient.delete<{ message: string }>(`/me/${houseId}`)
+  const response = await apiClient.delete<{ message: string }>(
+    `/me/?houseId=${houseId}`,
+  )
   return response.data
 }
