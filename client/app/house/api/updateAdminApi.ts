1610c1a2e5a3bbf12aab41b7d9c683a44922f00f
Atsuki Kitada <canvanvam@gmail.com>
2024-04-02 18:59:57 -0700

    define in type file

--- client/app/house/api/updateAdminApi.ts
+++ client/app/house/api/updateAdminApi.ts
@@ -1,10 +1,5 @@
 import apiClient from '@/_api/base'
-
-type UpdateAdminType = {
-  userId: number
-  houseId: number
-  isAdmin: boolean
-}
+import { UpdateAdminType } from '../type'
 
 export const updateAdminApi = async (data: UpdateAdminType) => {
   const response = await apiClient.put(`/user-houses/admin`, data)
