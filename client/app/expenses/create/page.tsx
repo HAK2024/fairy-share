f4d557ac4ce8f253cad0f8d80ba102070400ea5c
Atsuki Kitada <canvanvam@gmail.com>
2024-04-05 16:39:48 -0700

    change page name

--- client/app/expenses/create/page.tsx
+++ client/app/expenses/create/page.tsx
@@ -4,7 +4,7 @@ import { FormContainer } from '@/_components/layout'
 import { Heading } from '@/_components/ui'
 import { CreateExpenseForm } from '../components'
 
-export default function ExpenseCreatePage() {
+export default function CreateExpensePage() {
   return (
     <FormContainer>
       <Heading title='Create Expense Item' />
