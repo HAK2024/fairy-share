8735c586a4091283f17de14015f0e86af32e5d93
Atsuki Kitada <canvanvam@gmail.com>
2024-04-08 16:10:45 -0700

    fix path

--- client/app/expenses/[id]/edit/page.tsx
+++ client/app/expenses/[id]/edit/page.tsx
@@ -2,8 +2,8 @@
 
 import { FormContainer, Loading } from '@/_components/layout'
 import { Heading } from '@/_components/ui'
-import { EditExpenseForm } from '@/expenses/components'
-import { useGetExpenseQuery } from '@/expenses/hooks/api'
+import { EditExpenseForm } from '../../components'
+import { useGetExpenseQuery } from '../../hooks/api'
 
 export default function EditExpense({ params }: { params: { id: string } }) {
   const expenseId = Number(params.id)
