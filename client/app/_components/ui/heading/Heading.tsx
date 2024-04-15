553dcf24bb4ebd09d9f85ed231d6e37376a6d457
Hyejin Seok <hyejin.seok7@gmail.com>
2024-04-15 14:59:29 -0700

    Add align items ceneter for the heading

--- client/app/_components/ui/heading/Heading.tsx
+++ client/app/_components/ui/heading/Heading.tsx
@@ -8,7 +8,7 @@ interface HeadingProps {
 
 const Heading = ({ title, buttonComponent }: HeadingProps) => {
   return (
-    <div className='flex justify-between'>
+    <div className='flex items-center justify-between'>
       <h1 className='text-2xl font-bold text-teal-800 md:text-4xl'>{title}</h1>
       {buttonComponent && buttonComponent}
     </div>
