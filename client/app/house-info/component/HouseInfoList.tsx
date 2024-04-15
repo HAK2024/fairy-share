8df63529543e211e671446e296f1608caaca816f
Hyejin Seok <hyejin.seok7@gmail.com>
2024-04-15 14:48:02 -0700

    Add icon colors in house info page

--- client/app/house-info/component/HouseInfoList.tsx
+++ client/app/house-info/component/HouseInfoList.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { FiUser } from 'react-icons/fi'
-import { MdPushPin } from 'react-icons/md'
+import { IconType } from 'react-icons/lib'
+import { MdAccountCircle, MdPushPin } from 'react-icons/md'
+import { colorMap } from '@/_consts'
 import { RuleType } from '@/_types'
 
 type HouseInfoListProps = {
   rules: RuleType[]
-  members: { id: number; name: string }[]
+  members: { id: number; name: string; icon: IconType }[]
 }
 
 const HouseInfoList = ({ rules, members }: HouseInfoListProps) => {
@@ -15,7 +16,7 @@ const HouseInfoList = ({ rules, members }: HouseInfoListProps) => {
       <div className='mt-2 flex flex-col gap-3 rounded-md border border-amber-400 bg-amber-100 px-2 py-4 md:mt-4 md:px-7 md:py-7'>
         {rules.map((rule) => (
           <div key={rule.id} className='flex items-center'>
-            <MdPushPin className='mr-2 h-auto w-5 min-w-5 text-teal-700 md:mr-3 md:w-6 md:min-w-6' />
+            <MdPushPin className='mr-2 h-auto w-5 min-w-5 text-teal-700 md:mr-3 md:w-7 md:min-w-7' />
             <span className='text-sm font-semibold md:text-lg'>
               {rule.text}
             </span>
@@ -33,7 +34,9 @@ const HouseInfoList = ({ rules, members }: HouseInfoListProps) => {
       <div className='mt-2 flex flex-col gap-3 rounded-md border border-amber-400 bg-amber-100 px-2 py-4 md:mt-4 md:px-7 md:py-7'>
         {members.map((member) => (
           <div key={member.id} className='flex items-center'>
-            <FiUser className='mr-2 h-auto w-5 min-w-5 text-teal-700 md:mr-3 md:w-6 md:min-w-6' />
+            <MdAccountCircle
+              className={`mr-2 h-auto w-5 min-w-5 md:mr-3 md:w-7 md:min-w-7 ${colorMap[member.icon.toString()]}`}
+            />
             <span className='text-sm font-semibold md:text-lg'>
               {member.name}
             </span>
