b19e7edb7d1cab320de058afff2228e85f9804ce
Atsuki Kitada <canvanvam@gmail.com>
2024-04-25 12:37:44 -0700

    fix URL

--- server/src/libs/google-auth.ts
+++ server/src/libs/google-auth.ts
@@ -3,5 +3,5 @@ import { OAuth2Client } from 'google-auth-library';
 export const oAuth2Client = new OAuth2Client(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  'http://localhost:3000',
+  process.env.CLIENT_URL,
 );
